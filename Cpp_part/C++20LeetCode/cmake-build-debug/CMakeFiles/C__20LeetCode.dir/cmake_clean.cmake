aedb1cc4de38cc6b97654a3a9227a8ae29c1bcfd
Allen-C-Guan <54201792+allen-c-guan@users.noreply.github.com>
2021-10-10 23:19:12 +0800

    update

---
+++ Cpp_part/C++20LeetCode/cmake-build-debug/CMakeFiles/C__20LeetCode.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C__20LeetCode.dir/main.cpp.o"
+  "C__20LeetCode"
+  "C__20LeetCode.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C__20LeetCode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Cpp_part/C++20LeetCode/main.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Allen on 2021/10/10.
+//
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+
+int main() {
+
+    return 0;
+}
+
